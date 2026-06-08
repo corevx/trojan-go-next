86f4e322f34d6edeef73e638329e2b25e94d2d56
corevx <corevx@users.noreply.github.com>
2026-06-08 23:48:34 +0800

    fix: 修复 CI lint 配置和 shadowsocks 测试超时

--- tunnel/shadowsocks/shadowsocks_test.go
+++ tunnel/shadowsocks/shadowsocks_test.go
@@ -3,11 +3,13 @@ package shadowsocks
 import (
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"strings"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/p4gefau1t/trojan-go/common"
 	"github.com/p4gefau1t/trojan-go/config"
@@ -50,40 +52,68 @@ func TestShadowsocks(t *testing.T) {
 	s, err := NewServer(ctx, tcpServer)
 	common.Must(err)
 
+	t.Cleanup(func() {
+		c.Close()
+		s.Close()
+	})
+
 	wg := sync.WaitGroup{}
 	wg.Add(2)
+	errChan := make(chan error, 2)
 	var conn1, conn2 net.Conn
 	go func() {
+		defer wg.Done()
 		var err error
 		conn1, err = c.DialConn(nil, nil)
-		common.Must(err)
-		conn1.Write(util.GeneratePayload(1024))
-		wg.Done()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		conn1.SetDeadline(time.Now().Add(5 * time.Second))
+		if _, err := conn1.Write(util.GeneratePayload(1024)); err != nil {
+			errChan <- err
+		}
 	}()
 	go func() {
+		defer wg.Done()
 		var err error
 		conn2, err = s.AcceptConn(nil)
-		common.Must(err)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		conn2.SetDeadline(time.Now().Add(5 * time.Second))
 		buf := [1024]byte{}
-		conn2.Read(buf[:])
-		wg.Done()
+		if _, err := io.ReadFull(conn2, buf[:]); err != nil {
+			errChan <- err
+		}
 	}()
 	wg.Wait()
+	close(errChan)
+	for err := range errChan {
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	conn1.SetDeadline(time.Now().Add(5 * time.Second))
+	conn2.SetDeadline(time.Now().Add(5 * time.Second))
 	if !util.CheckConn(conn1, conn2) {
 		t.Fail()
 	}
 
+	acceptErr := make(chan error, 1)
 	go func() {
-		var err error
-		conn2, err = s.AcceptConn(nil)
+		_, err := s.AcceptConn(nil)
 		if err == nil {
-			t.Fail()
+			err = fmt.Errorf("expected invalid shadowsocks connection")
 		}
+		acceptErr <- err
 	}()
 
 	// test redirection
 	conn3, err := tcpClient.DialConn(nil, nil)
 	common.Must(err)
+	conn3.SetDeadline(time.Now().Add(5 * time.Second))
 	n, err := conn3.Write(util.GeneratePayload(1024))
 	common.Must(err)
 	fmt.Println("write:", n)
@@ -94,8 +124,14 @@ func TestShadowsocks(t *testing.T) {
 	if !strings.Contains(string(buf[:n]), "Bad Request") {
 		t.Fail()
 	}
+	select {
+	case err := <-acceptErr:
+		if err == nil {
+			t.Fatal("expected invalid shadowsocks connection")
+		}
+	case <-time.After(5 * time.Second):
+		t.Fatal("timed out waiting for invalid shadowsocks connection")
+	}
 	conn1.Close()
 	conn3.Close()
-	c.Close()
-	s.Close()
 }
